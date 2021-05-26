9dd85ee3263e2945d5033152594d70595b0a2351
Bongjungi <83949824+Bongjungi@users.noreply.github.com>
2021-05-26 14:26:46 +0900

    Add files via upload

---
+++ JAVA/ch01/src/InitializeUseExPublic.java
@@ -0,0 +1,10 @@
+
+public class InitializeUseExPublic {
+	public static void main(String[] args) {
+		//int a;
+		//System.out.println(a);     -----      불가능 a 초기화필요함.
+		ExPublic ex = new ExPublic();
+		System.out.println(ex.power);
+	
+	}
+}
