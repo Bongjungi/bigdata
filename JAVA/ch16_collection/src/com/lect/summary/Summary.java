5cd7f729c943e52809b55b79ac6cc16270167309
Bongjungi <83949824+Bongjungi@users.noreply.github.com>
2021-05-27 13:07:26 +0900

    Delete JAVA/ch16_collection directory

--- JAVA/ch16_collection/src/com/lect/summary/Summary.java
+++
@@ -1,9 +0,0 @@
-package com.lect.summary;
-
-public class Summary {
-	//List 순서가 있는 데이터의 집합, 데이터의 중복을 허용한다. 구현클래스 : ArrayList,LinkedList,Stack,Vector 등
-	//Set 순서를 유지하지 않는 데이터의 집합, 데이터의 중복을 허용하지 않는다. 구현클래스 : HashSet, TreeSet 등
-	//Map 키(Key) 값(Value) 쌍으로 이루어진 데이터의 집합, 순서가 유지되지 않으며, 키는 중복을 허용하지 않고 값은 중복을 허용한다. 구현클래스 : HashMap, HashTable 등
-	
-	// Iterator<키의 형태> iterator = hashmap.keySet().iterator();
-}
