9fae179a7887fc82786ad79487b90073ccec8829
Bongjungi <83949824+Bongjungi@users.noreply.github.com>
2021-06-03 18:11:31 +0900

    Add files via upload

---
+++ JAVA/JavaLecture/ch03_ctrl/src/com/lec/loopandcondtion/While_dowhile.java
@@ -0,0 +1,23 @@
+package com.lec.loopandcondtion;
+
+public class While_dowhile {
+	public static void main(String[] args) {
+		int b=1;
+		while(b<=20) {
+			b++;
+		}
+		do {
+			b++;
+		}while(b<=20);
+	/*	do {
+			b++;
+		}while(true)
+		while(true){
+			b++
+		}
+		for(;;){
+			b++;
+		}
+		*/ //무한루프
+	}
+}
