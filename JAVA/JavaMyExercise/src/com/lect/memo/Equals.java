7ba4bdf761a3e24efdb47f253131f2ace578f172
bjk930 <83949824+bjk930@users.noreply.github.com>
2021-06-09 10:31:39 +0900

    Delete JAVA/JavaMyExercise directory

--- JAVA/JavaMyExercise/src/com/lect/memo/Equals.java
+++
@@ -1,9 +0,0 @@
-package com.lect.memo;
-
-public class Equals {
-	// object의 equals는 주소를 비교한다.
-	// string의 equals는 주소 안의 값을 비교한다.
-	// tostring은 object에서 상속받는다. 당연히 stringbuilder에서 쓰는 tostring이랑 다르다.
-	// new StringBuilder(new String(new char[])).toString()
-
-}
