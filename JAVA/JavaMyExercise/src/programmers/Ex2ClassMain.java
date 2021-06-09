392aeeb969967bf882426ab899e8be00fbfcbc29
bjk930 <83949824+bjk930@users.noreply.github.com>
2021-06-09 10:33:57 +0900

    Add files via upload

---
+++ JAVA/JavaMyExercise/src/programmers/Ex2ClassMain.java
@@ -0,0 +1,9 @@
+package com.lect.programmers;
+
+public class Ex2ClassMain {
+	public static void main(String[] args) {
+		Ex2Class ex = new Ex2Class();
+		int[] nums = {1,2,4,3,2,4,5,7,8,4,6,5,2,1,5,4,5,6,5,4,2,3,4,5,8,7,5,6,12};
+		System.out.println(ex.solution(nums));
+	}
+}
