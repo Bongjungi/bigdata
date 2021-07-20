d404c7dd95c654a6add7bd7a8954e122ade995bc
bjk930 <83949824+bjk930@users.noreply.github.com>
2021-07-20 15:00:06 +0900

    Delete LectureNote/JSP directory

--- LectureNote/JSP/ex/src/ex/IIdPw.java
+++
@@ -1,9 +0,0 @@
-package ex;
-
-public interface IIdPw {
-	int idConfirm(String id);
-	int login(String id, String pw);
-	String pwget(String id);
-	int idpwinsert(String id, String pw, String tel, String address, String gender, String email);
-	String chChar(int e, int n, String str);
-}
